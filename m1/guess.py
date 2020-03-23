8e1ee179e094d3fbed8052435fa911e533b62b6c
Feng Tian <fengttt@gmail.com>
2020-03-22 22:32:52 -0700

    Adding m1 scripts.

---
+++ m1/guess.py
@@ -0,0 +1,26 @@
+import random
+
+if __name__ == '__main__':
+    max = 100
+    secret = random.randint(1, max)
+    cnt = 0
+    while True:
+        cnt += 1
+        print("Guess a number in 1 -", max)
+        val = input()
+        val = int(val)
+
+        if val == secret:
+            print("You guessed it right!")
+            break
+        elif val < secret:
+            print("\tYou guessed", val, ", which is too small!")
+        else:
+            print("\tYou guessed", val, ", which is too big!")
+
+        print("\tYou have guessed", cnt, "times.")
+
+
+
+
+
